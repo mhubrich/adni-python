0236c2dbf4560d8943323be1135f09d1a0364916
Markus Hubrich <mhubrich@students.uni-mainz.de>
2016-10-14 11:14:21 +0200

    training and validation IDs from mogon

--- utils/split_scans.py
+++ utils/split_scans.py
@@ -10,21 +10,21 @@ def split_scans(scans, split, names, pathOut, classes=None):
         "Sum of splits is not equal to 1.0."
     assert len(split) == len(names), \
         "Number of splits is not equal to number of provided names."
-
+    
     imageID_split = []
     for _ in split:
         imageID_split.append([])
-
+    
     maxValue = 999999999
     maxCounts = np.full(len(split), maxValue, dtype=np.int)
-
+    
     groups, names = sort_groups(scans)
     minGroup = maxValue
     for g in groups:
         if len(g) < minGroup:
             minGroup = len(g)
     maxCounts[0] = minGroup * split[0]
-
+    
     indices = np.zeros(len(groups), dtype=np.int)
     for i in range(split):
         for g in groups:
