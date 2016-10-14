8b2520ce8cb10527c5231bbe9196cf715979b643
Markus Hubrich <mhubrich@students.uni-mainz.de>
2016-10-14 19:08:39 +0200

    bug fix

--- utils/config.py
+++ utils/config.py
@@ -1,17 +1,24 @@
+import os
 import pickle
 
 
-def save_config(path='config.p'):
-    config = {'nii': True,
-              'ADNI': '/home/mhubrich/ADNI',
-              'load_all_scans': True
+def save_config(path=None):
+    config = {'nii': False,
+              'ADNI': '/home/mhubrich/ADNI_npy',
+              'load_all_scans': False
               }
-    pickle.dump(config, open(path, 'wb'))
+    if path is None:
+        pickle.dump(config, open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.p'), 'wb'))
+    else:
+        pickle.dump(config, open(path, 'wb'))
 
 
-def load_config(path='config.p'):
+def load_config(path=None):
     try:
-        return pickle.load(open(path, 'rb'))
+        if path is None:
+            return pickle.load(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.p'), 'rb'))
+        else:
+            return pickle.load(open(path, 'rb'))
     except IOError:
         return None
 
