76de4ddc18fac2dff1e6a00dcadc5c8b3b33e34c
Markus Hubrich (Karwath) <mhubrich@students.uni-mainz.de>
2016-10-15 16:22:50 +0200

    snyc

--- cnn/keras/partitions/predictions/predict.py
+++ cnn/keras/partitions/predictions/predict.py
@@ -1,20 +1,22 @@
-from cnn.keras.models.partitions1.model import build_model
+from cnn.keras.models.partitions4.model import build_model
 from cnn.keras.prediction.predict_generator import predict_generator
 from cnn.keras.partitions.preprocessing.image_processing import inputs
-from cnn.keras.partitions.train import _split_scans, classes
 from cnn.keras.utils.write_prediction import write_prediction
+from utils.split_scans import read_imageID
+from utils.config import config
 
 
+classes = ['Normal', 'AD']
 target_size = (29, 29, 29)
-batch_size = 128
-load_all_scans = True
-path_weights = '/home/mhubrich/checkpoints/adni/partitions1_2/weights.506-loss_0.399-acc_0.831.h5'
-output_name = 'part1_2_506-5_5'
+batch_size = 16
+load_all_scans = False
+path_weights = '/home/mhubrich/weights.795-loss_0.362-acc_0.849.h5'
+output_name = 'part4_1_795-5_5.csv'
 
 interval_x = range(7, 86+2 - target_size[0], 5)
 interval_y = range(3, 92+2 - target_size[1], 5)
-interval_z1 = [15, 16, 17, 18, 19]
-interval_z2 = [48, 49, 50, 51, 52]
+interval_z1 = [15, 17, 19, 21, 23]
+interval_z2 = [43, 45, 47, 49, 51]
 grid1 = [(x, y, z) for x in interval_x for y in interval_y for z in interval_z1]
 grid2 = [(x, y, z) for x in interval_x for y in interval_y for z in interval_z2]
 grid = [grid1, grid2]
@@ -22,7 +24,7 @@ grid = [grid1, grid2]
 
 def predict():
     # Get inputs for labeling
-    _, scans_test = _split_scans()
+    scans_test = read_imageID(config['ADNI'], '/home/mhubrich/mogon_val')
     test_inputs = inputs(scans_test, target_size, grid, batch_size, load_all_scans, classes, 'predict')
 
     # Set up the model
@@ -34,8 +36,8 @@ def predict():
     pred, filenames = predict_generator(model,
                                         test_inputs,
                                         val_samples=test_inputs.nb_sample,
-                                        max_q_size=640,
-                                        nb_preprocessing_threads=1)
+                                        max_q_size=64,
+                                        nb_preprocessing_threads=2)
 
     return pred, filenames
 
