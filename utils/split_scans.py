b00eb8a4ccd83ce246b0cd80b42355bae73d98f1
Markus Hubrich <mhubrich@students.uni-mainz.de>
2016-12-02 17:44:00 +0100

    update

--- utils/split_scans.py
+++ utils/split_scans.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-#from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.model_selection import StratifiedKFold, train_test_split
 
 from utils.load_scans import load_scans
 from utils.sort_scans import sort_subjects
@@ -23,6 +23,72 @@ def write_imageID(scans, fname):
     with open(fname, 'wb') as f:
         for scan in scans:
             f.write(scan.imageID + os.linesep)
+
+
+def CV_AAL(scans, k, val_split, classes, path, seed=None):
+    num_intervals = 5
+    indices = ['AAL64', 'AAL65', 'AAL34', 'AAL35', 'AAL61']
+    class_indices = dict(zip(classes, np.arange(len(classes))))
+    means = {}
+    subjects, subject_names = sort_subjects(scans)
+    for n in subject_names:
+        counts = {}
+        m = 0.0
+        flag = False
+        for scan in subjects[n]:
+            if scan.group in classes:
+                flag = True
+                if scan.group in counts:
+                    counts[scan.group] += 1
+                else:
+                    counts[scan.group] = 1
+                for k in range(len(indices)):
+                    m += np.mean(np.load(scan.path.replace('AAL64', indices[k])))
+        if flag:
+            m /= np.sum(counts.values()) * len(indices)
+            if max(counts, key=counts.get) in means:
+                means[max(counts, key=counts.get)].append(m)
+            else:
+                means[max(counts, key=counts.get)] = [m]
+    ranges = {}
+    for key in means:
+        ranges[key] = []
+        for k in range(1, num_intervals):
+            ranges[key].append(np.sort(means[key])[(len(means[key])/num_intervals) * k])
+    x, y = [], []
+    for n in subject_names:
+        counts = {}
+        m = 0.0
+        flag = False
+        for scan in subjects[n]:
+            if scan.group in classes:
+                flag = True
+                if scan.group in counts:
+                    counts[scan.group] += 1
+                else:
+                    counts[scan.group] = 1
+                for k in range(len(indices)):
+                    m += np.mean(np.load(scan.path.replace('AAL64', indices[k])))
+        if flag:
+            x.append(n)
+            m /= np.sum(counts.values()) * len(indices)
+            interval = len(ranges[max(counts, key=counts.get)])
+            for i in range(len(ranges[max(counts, key=counts.get)])):
+                if m < ranges[max(counts, key=counts.get)][i]:
+                    interval = i
+                    break
+            y.append(class_indices[max(counts, key=counts.get)] * num_intervals + interval)
+    skf = StratifiedKFold(n_splits=k, random_state=seed, shuffle=True)
+    fold = 0
+    for index, test_index in skf.split(x, y):
+        fold += 1
+        # First, use set 'index' to generate train and val sets
+        y_index = [y[i] for i in index]
+        train_index, val_index = train_test_split(index, stratify=y_index, test_size=val_split, random_state=seed)
+        # Save train, val and test sets
+        write_imageID([s for i in train_index for s in subjects[x[i]]], os.path.join(path, str(fold) + '_train'))
+        write_imageID([s for i in val_index for s in subjects[x[i]]], os.path.join(path, str(fold) + '_val'))
+        write_imageID([s for i in test_index for s in subjects[x[i]]], os.path.join(path, str(fold) + '_test'))
 
 
 def CV(scans, k, val_split, classes, path, seed=None):
